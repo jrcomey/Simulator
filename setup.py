52042d4ff2c6d4ead9d7812ff1f0c6c50fb893f4
jrcomey <jack.rhys.comey@gmail.com>
2020-08-24 15:34:38 -0700

    Added setup file, unconfigured

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+	name = 'SimName'
+	version = '0.1'
+	description = 'Virtual UAV field testing'
+	author = 'Jack Comey'
+	author_email = 'jrcomey@ucdavis.edu'
+	packages = ['SimName']
+	install_requires = ['numpy, pandas']
+)
+
+
